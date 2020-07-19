8f6b46eb991bb424a46a5cc4b7d256d36131f395
g3kgirl <shikha.relieved@gmail.com>
2020-07-19 22:32:38 +0530

    form data to push post in posts array

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var data = require('./data.json');
 var { posts, name } = data;
-
-
 /* GET home page. */
+
 router.get('/', function (req, res, next) {
+
   res.render('index', {
     title: 'Express',
     name,
@@ -13,6 +13,17 @@ router.get('/', function (req, res, next) {
   });
 
 });
+
+router.post('/create', function (req, res, next) {
+
+  const { title, body } = req.body;
+
+  const post = { title, body };
+
+  posts = [post, ...posts];
+
+  res.redirect('/');
+})
 
 /* GET login page. */
 router.get('/login', function (req, res, next) {
