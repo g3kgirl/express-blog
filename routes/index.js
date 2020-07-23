88fe70a0b25d901f8f175361affe7152f1cf5099
g3kgirl <shikha.relieved@gmail.com>
2020-07-23 21:34:35 +0530

    write to file system

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var data = require('./data.json');
-var { posts, name } = data;
+var fs = require('fs');
+const { json } = require('body-parser');
+
 /* GET home page. */
 
 router.get('/', function (req, res, next) {
+
+  // var data = require('./data.json');
+  var filedata = fs.readFileSync('data.json');
+  var data = JSON.parse(filedata);
+
+  var { posts, name } = data;
 
   res.render('index', {
     title: 'Express',
@@ -20,9 +27,25 @@ router.post('/create', function (req, res, next) {
 
   const post = { title, body };
 
-  posts = [post, ...posts];
+  // const oldData = require('./data.json');
 
-  res.redirect('/');
+  var filedata = fs.readFileSync('data.json');
+  var oldData = JSON.parse(filedata);
+
+  const { posts } = oldData;
+
+  const newPosts = [post, ...posts];
+
+  oldData.posts = newPosts;
+
+  let data = JSON.stringify(oldData, null, 2);
+
+  fs.writeFile('data.json', data, (err, data) => {
+    console.log(err, data);
+    res.status(302).redirect('/');
+  });
+
+
 })
 
 /* GET login page. */
